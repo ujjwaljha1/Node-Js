88d418e3a60812dd72ed8bfbeee04c4f7ffe29df
Ujjwal <ujjwaljha744@gmail.com>
2024-07-01 22:40:13 +0530

    Query

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.get("/", (req, res) => {
 // JSON route
 app.get("/api/phone", (req, res) => {
   const newItem = products.map((product) => {
-    const { id, firstName, gmail } = product;
-    return { id, firstName, gmail };
+    const { id, title, gmail } = product;
+    return { id, title, gmail };
   });
   res.json(newItem);
 });
 
-// Param route
+// Param route by Gmail
 app.get("/api/gmail/:gmail", (req, res) => {
   const { gmail } = req.params;
-  const singleProduct = products.find((product) => product.gmail === gmail);
-  
+  const singleProduct = products.find((product) => product.meta.reviewerEmail === gmail);
+
   if (singleProduct) {
     res.json(singleProduct);
   } else {
@@ -29,17 +29,35 @@ app.get("/api/gmail/:gmail", (req, res) => {
   }
 });
 
-//Param Examaple: 2
+// Param route by ID
 app.get("/api/product/:id", (req, res) => {
-    const { id } = req.params;
-    const singleProduct = products.find((product) => product.id === parseInt(id));
-    
-    if (singleProduct) {
-      res.json(singleProduct);
-    } else {
-      res.status(404).send('Product not found');
-    }
-  });
+  const { id } = req.params;
+  const singleProduct = products.find((product) => product.id === parseInt(id));
+
+  if (singleProduct) {
+    res.json(singleProduct);
+  } else {
+    res.status(404).send('Product not found');
+  }
+});
+
+// Query route
+app.get("/api/find/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => 
+      product.title.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, parseInt(limit));
+  }
+
+  res.json(sortedProducts);
+});
 
 app.get("/about", (req, res) => {
   res.send("About page");
